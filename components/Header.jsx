c7bc6c5d606d03a6dbbb86fd8df3c49d47a0021b
avelous <avelousujiri@gmail.com>
2022-07-13 21:35:21 +0100

    background-edit

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,8 +2,8 @@ import { ConnectButton } from "web3uikit"
 
 export default function Header() {
     return (
-        <div className="border-b-2 flex flex-row">
-            <h1 className="py-4 px-4 text-2xl hover:-rotate-2 text-white ">
+        <div className="border-b-2 flex flex-row   border-stone-900 h-15 max-h-18">
+            <h1 className="py-4 px-4 mt-2 text-4rem hover:-rotate-2 font-semibold">
                 DECENTRALIZED LOTTERY
             </h1>
             <div className="ml-auto  py-4 px-4  ">
--- pages/index.js
+++ pages/index.js
@@ -7,19 +7,19 @@ import LotteryEntrance from "../components/lotteryEntrance"
 
 export default function Home() {
     return (
-        <div >
+      
             <div
-                className={styles.container}
-                style={{
-                    backgroundImage:
-                        "url(" +
-                        "https://media.istockphoto.com/photos/empty-dark-blue-studio-room-with-light-and-shadow-abstract-background-picture-id1339787499?b=1&k=20&m=1339787499&s=170667a&w=0&h=psnH04yZpoVzxa4xTZt4hDwff5WiO0cQrtnYdRttkSI=" +
-                        ")",
-                    backgroundPosition: "center",
-                    backgroundSize: "cover",
-                    backgroundRepeat: "no-repeat",
-                    height: "100%",
-                }}
+                className="flexbox w-screen bg-cyan-800 px-4"
+                // style={{
+                //     backgroundImage:
+                //         "url(" +
+                //         "https://media.istockphoto.com/photos/empty-dark-blue-studio-room-with-light-and-shadow-abstract-background-picture-id1339787499?b=1&k=20&m=1339787499&s=170667a&w=0&h=psnH04yZpoVzxa4xTZt4hDwff5WiO0cQrtnYdRttkSI=" +
+                //         ")",
+                //     backgroundPosition: "center",
+                //     backgroundSize: "cover",
+                //     backgroundRepeat: "no-repeat",
+                //     height: "100%",
+                // }}
             >
                 <Head>
                     <title>Smart Contract Lottery</title>
@@ -35,6 +35,6 @@ export default function Home() {
                     <LotteryEntrance />
                 </div>
             </div>
-        </div>
+   
     )
 }
