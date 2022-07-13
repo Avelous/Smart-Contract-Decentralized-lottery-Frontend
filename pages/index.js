a64fab84bcb53c2ebb9b686bea55cd43c04a017e
avelous <avelousujiri@gmail.com>
2022-07-13 21:35:47 +0100

    background-edit

--- pages/index.js
+++ pages/index.js
@@ -10,16 +10,7 @@ export default function Home() {
       
             <div
                 className="flexbox w-screen bg-cyan-800 px-4"
-                // style={{
-                //     backgroundImage:
-                //         "url(" +
-                //         "https://media.istockphoto.com/photos/empty-dark-blue-studio-room-with-light-and-shadow-abstract-background-picture-id1339787499?b=1&k=20&m=1339787499&s=170667a&w=0&h=psnH04yZpoVzxa4xTZt4hDwff5WiO0cQrtnYdRttkSI=" +
-                //         ")",
-                //     backgroundPosition: "center",
-                //     backgroundSize: "cover",
-                //     backgroundRepeat: "no-repeat",
-                //     height: "100%",
-                // }}
+             
             >
                 <Head>
                     <title>Smart Contract Lottery</title>
